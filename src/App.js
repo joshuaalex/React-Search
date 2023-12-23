5319353ddcc13ff1bee285e09b6ca7ceb9789d7d
joshuaalex <joshuaalex52@gmail.com>
2023-12-23 01:08:35 +0100

    completed search

--- src/App.js
+++ src/App.js
@@ -6,26 +6,50 @@ import { MovieCard } from './components/MovieCard/MovieCard';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [error, setError] = useState(null);
+
+  const fetchMovies = async (searchQuery) => {
+    try {
+      const { data } = await axios.get(`http://www.omdbapi.com/?s=${searchQuery}&apikey=6451d5a9`);
+      if (data && data.Search) {
+        setMovies(data.Search);
+        console.log(data.Search);
+      } else {
+        setMovies([]);
+      }
+      setError(null);
+    } catch (error) {
+      setError('Error fetching movies. Please try again.');
+      setMovies([]);
+    }
+  };
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const { data } = await axios.get("http://www.omdbapi.com/?s=guardians&apikey=6451d5a9"); 
-        if (data && data.Search) {
-          setMovies(data.Search); 
-          console.log(data.Search);
-        }
-      } catch (error) {
-        console.error('Error fetching movies:', error);
-      }
-    };
+    if (searchTerm.trim() !== '') {
+      fetchMovies(searchTerm);
+    }
+  }, [searchTerm]);
 
-    fetchMovies();
-  }, []);
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div className="App">
-      <h1>Movies</h1>
+      <header className='header'>
+        <h1 className='heading-1'>Movies</h1>
+      </header>
+      <div className="search-container">
+        <input
+          className='search-input'
+          type="text"
+          placeholder="Search movies..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      {error && <p>{error}</p>}
       <div className="movies-container">
         {movies.length > 0 ? (
           movies.map(movie => (
